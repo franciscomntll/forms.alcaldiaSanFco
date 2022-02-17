29f31c772139231d9923622aa2c238b7abfc1337
Francisco <ciscomontilla@gmail.com>
2022-02-16 22:32:26 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div style={{width: "100vw", height: "100vh", overflow: "hidden", margin: 0, padding: 0}}>
+            <iframe id="JotFormIFrame-220467632082655" title="App" src="https://www.jotform.com/app/220467632082655?appEmbedded=1" style={{ width: "100%", height: "100%", border: 0}}></iframe>
+        </div>
+    )
+}
+
+export default Home
